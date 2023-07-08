359d58335b9719515c4000fe0da5444fb3b2bc59
bijou <igor.slobodarski@gmail.com>
2023-07-08 18:23:10 +0200

    belj

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { exampleRouter } from "y/server/api/routers/example";
 import { createTRPCRouter } from "y/server/api/trpc";
+import { testRouter } from "./routers/test";
 
 /**
  * This is the primary router for your server.
@@ -7,6 +8,7 @@ import { createTRPCRouter } from "y/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  testRouter: testRouter,
   example: exampleRouter,
 });
 
